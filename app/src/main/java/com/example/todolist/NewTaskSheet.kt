5d4d5eb11a3c4829ff6e35ea350812bfa3176bde
Dinithi Wickramaarachchi <dinithiwr@gmail.com>
2024-05-12 02:31:27 +0530

    New Task Sheet created

---
+++ app/src/main/java/com/example/todolist/NewTaskSheet.kt
@@ -0,0 +1,41 @@
+package com.example.todolist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.databinding.FragmentNewTaskSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class NewTaskSheet : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentNewTaskSheetBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
+        binding.saveButton.setOnClickListener{
+            saveAction()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun saveAction() {
+        taskViewModel.name.value = binding.name.text.toString()
+        taskViewModel.desc.value = binding.desc.text.toString()
+        binding.name.setText("")
+        binding.desc.setText("")
+        dismiss()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentNewTaskSheetBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/todolist/TaskViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.todolist
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TaskViewModel: ViewModel() {
+    var name = MutableLiveData<String>()
+    var desc = MutableLiveData<String>()
+}
